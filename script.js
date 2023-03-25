bee3489eafb33caf07851a41e5a78787d8f01b7b
Gangadhar Reddy <117704156+Gangadhar0022@users.noreply.github.com>
2023-03-25 11:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-window.promises = [];
+//window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+const promises = [
+  new Promise((resolve) => setTimeout(() => resolve(1), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve(2), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve(3), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve(4), Math.floor(Math.random() * 5000) + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve(5), Math.floor(Math.random() * 5000) + 1000)),
+];
+
+Promise.any(promises).then((result) => {
+  document.getElementById("output").innerHTML = result;
+});
